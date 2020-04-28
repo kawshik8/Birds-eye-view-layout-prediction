848dc88c4308ffe040d30e5f9e47d14e49a45baf
kawshik <kawshik98@gmail.com>
2020-04-28 17:49:43 -0400

    Update visualize.py

--- notebooks/visualize.py
+++ notebooks/visualize.py
@@ -91,62 +91,65 @@ for batch, inputs in enumerate(trainloader):
         batch_input = {"image":image,"idx":index, "bbox":bounding_box, "classes":classes, "action":action, "ego":ego, "road":road}
 
         batch_output = model(batch_input)
+        if args.detect_objects:
+            boxes = batch_output["boxes"].detach().numpy()
+            classes = batch_output["classes"].detach().numpy()
         
-        boxes = batch_output["boxes"].detach().numpy()
-        classes = batch_output["classes"].detach().numpy()
-        
-        print(boxes.shape)
-        print(classes.shape)
+            print(boxes.shape)
+            print(classes.shape)
         
         road = road.view(256,256)
 #         road[road >= 0.5] = 1
 #         road[road < 0.5] = 0
 
-        fig, ax = plt.subplots()
-
-        ax.imshow(road, cmap ='binary');
-
-        # The ego car position
-        ax.plot(128, 128, 'x', color="red")
-
-        for i, bb in enumerate(batch_input['bbox'][0]):
-            # You can check the implementation of the draw box to understand how it works 
-#             print(bb)
-            bb = bb.unsqueeze(0)
-#             print(batch_input['classes'][0][i][0])
-            bb = torch.cat([bb[:,2],bb[:,2],bb[:,0],bb[:,0],bb[:,1],bb[:,3],bb[:,1],bb[:,3]]).view(2,4)
-            bb = ((((bb * 800)/256) - 400 )/10)*256/800
-            draw_box(ax, bb, color=color_list[int(batch_input['classes'][0][i][0])])    
         
-       
-        plt.savefig("./generated/" + str(file) + "_gt_road_boxes_" + str(batch) + ".jpg")
+        road1 = batch_output["road_map"].view(256,256) 
+
+        if args.detect_objects:
+            fig, ax = plt.subplots()
+
+            ax.imshow(road, cmap ='binary');
+
+            # The ego car position
+            ax.plot(128, 128, 'x', color="red")
+
+            for i, bb in enumerate(batch_input['bbox'][0]):
+                # You can check the implementation of the draw box to understand how it works 
+    #             print(bb)
+                bb = bb.unsqueeze(0)
+    #             print(batch_input['classes'][0][i][0])
+                bb = torch.cat([bb[:,2],bb[:,2],bb[:,0],bb[:,0],bb[:,1],bb[:,3],bb[:,1],bb[:,3]]).view(2,4)
+                bb = ((((bb * 800)/256) - 400 )/10)*256/800
+                draw_box(ax, bb, color=color_list[int(batch_input['classes'][0][i][0])])    
+
+
+            plt.savefig("./generated/" + str(file) + "_gt_road_boxes_" + str(batch) + ".jpg")
+
+#         ['fl_x', 'fr_x', 'bl_x', 'br_x', 'fl_y', 'fr_y','bl_y', 'br_y']
         
-        ['fl_x', 'fr_x', 'bl_x', 'br_x', 'fl_y', 'fr_y','bl_y', 'br_y']
-        
-        road1 = batch_output["road_map"].view(256,256)
 
-        fig, ax = plt.subplots()
+            fig, ax = plt.subplots()
 
-        ax.imshow(road1.detach().numpy(), cmap ='binary');
+            ax.imshow(road1.detach().numpy(), cmap ='binary');
 
-        # The ego car position
-        ax.plot(128, 128, 'x', color="red")
+            # The ego car position
+            ax.plot(128, 128, 'x', color="red")
 
-        for i, bb in enumerate(batch_output["boxes"][0]):
-            # You can check the implementation of the draw box to understand how it works 
-#             print(bb.shape)
-            bb = bb.unsqueeze(0).detach().clone()
-            if(len(bb[bb!=0])>0):
-                print(bb)
-            
-            bb = torch.cat([bb[:,2],bb[:,2],bb[:,0],bb[:,0],bb[:,1],bb[:,3],bb[:,1],bb[:,3]]).view(2,4)
-            bb = ((((bb * 800)/256) - 400 )/10)*256/800
-#             print(batch_output["classes"][0][i])
-#             print(int(batch_output["classes"][0][i].max(dim=0)[1]))
-            draw_box(ax, bb, color=color_list[int(batch_output["classes"][0][i].max(dim=0)[1].detach().clone())])    
-        
-       
-        plt.savefig("./generated/" + str(file) + "_gen_road_boxes_" + str(batch) + ".jpg")
+            for i, bb in enumerate(batch_output["boxes"][0]):
+                # You can check the implementation of the draw box to understand how it works 
+    #             print(bb.shape)
+                bb = bb.unsqueeze(0).detach().clone()
+                if(len(bb[bb!=0])>0):
+                    print(bb)
+
+                bb = torch.cat([bb[:,2],bb[:,2],bb[:,0],bb[:,0],bb[:,1],bb[:,3],bb[:,1],bb[:,3]]).view(2,4)
+                bb = ((((bb * 800)/256) - 400 )/10)*256/800
+    #             print(batch_output["classes"][0][i])
+    #             print(int(batch_output["classes"][0][i].max(dim=0)[1]))
+                draw_box(ax, bb, color=color_list[int(batch_output["classes"][0][i].max(dim=0)[1].detach().clone())])    
+
+
+            plt.savefig("./generated/" + str(file) + "_gen_road_boxes_" + str(batch) + ".jpg")
         #         print("road",np.unique(road))
         
 #         road1[road1 >= 0.5] = 1
